fc46018b3f343d9714eca8a02dc91c1aa6a22529
= <=>
2023-11-13 20:50:16 +0530

    ENV Config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env : {
-        BEARER_TOKEN: process.env.BEARER_TOKEN
+        BEARER_TOKEN: process.env.BEARER_TOKEN,
+        CYCLIC_URL: process.env.CYCLIC_URL
     }
 }
 
